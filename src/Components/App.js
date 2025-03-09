a15e5596fcdca879d595efcd5c9f004016a4c078
Brittany Halterman <halterma@gmail.com>
2025-03-09 18:25:10 -0400

    Set the search query state

--- src/Components/App.js
+++ src/Components/App.js
@@ -9,24 +9,28 @@ const apiKey = process.env.REACT_APP_API_KEY;
 // console.log(apiKey);
 
 function App() {
-  const [data, setData] = useState([]); // declare state
+  const [data, setData] = useState([]);
+  const [query, setQuery] = useState('cats'); // declare new state
+
+  // update the query state
+  const performSearch = (value) => setQuery(value);
 
   useEffect(() => {
     axios(
-      `http://api.giphy.com/v1/gifs/search?q=${query}&limit=24&api_key=REACT_${apiKey}`
-        .then((response) => setData(response.data.data))
-        .catch((error) =>
-          console.log('Error fetching and parsing data', error)
-        )
-    );
-  }, []);
+      `http://api.giphy.com/v1/gifs/search?q=${query}&limit=24&api_key=${apiKey}`
+    )
+      .then((response) => setData(response.data.data))
+      .catch((error) =>
+        console.log('Error fetching and parsing data', error)
+      );
+  }, [query]); // add the query dependency
 
   return (
     <>
       <div className="main-header">
         <div className="inner">
           <h1 className="main-title">GifSearch</h1>
-          <SearchForm />
+          <SearchForm onSearch={performSearch} />
         </div>
       </div>
       <div className="main-content">
